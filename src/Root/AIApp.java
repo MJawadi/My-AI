abc6cf2f80986f42b0c0b6cf2e2427a22023ce84
Mohammad Jawadi <jawadi@corp.atacama.de>
2018-10-16 16:56:48 +0200

    update 218.10.16

--- src/Root/AIApp.java
+++ src/Root/AIApp.java
@@ -4,36 +4,37 @@ import java.util.Scanner;
 public class AIApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello!!");
+		
 	    Scanner input = new Scanner(System.in);
 	    String answer = input.nextLine();
-	    Scanner take  = new Scanner(System.in);
 	    boolean hasName = false;
         String name = new String();
         boolean Conversation = true;
         
-        
-          while(Conversation == true) {
+          System.out.println("Hello!!");
+          while(Conversation) {
         	  
               if(hasName == true) {
                   System.out.println("Hello human! My name is " + name);
               } 
               else {
            	      System.out.println("Hello there! My name is..!!! I dont have a name yet! Would you like to give me a name? ");
+                  answer = input.nextLine();
+          	      if ((answer.equals("Yes") || answer.equals("yes")) || (answer.equals("sure") || answer.equals("yeah")) );{
+      		          System.out.println("\nOOh GOOD! What you want to call me then???");
+      		          name = input.nextLine();
+      		          hasName = true;
+      		          System.out.println(name + "!!!" + "It's pretty!! I like it :)");
+      	          }
+          	      if (answer.equals("No") || answer.equals("no")){
+          	    	  System.out.println("\nWhy not!!! Maybe because you don't have imagnation or not Good to pick a name!!??");
+          	      }else if(answer.equals("I don't know") || answer.equals("i dont know")) {
+          	    	  
+          	      }
               }
-              
-              
-      	      if ((answer.equals("Yes") || answer.equals("yes")) || (answer.equals("sure") || answer.equals("yeah")) );{
-  		          System.out.println("\nOOh GOOD! What want to call me then???");
-  		          answer = name;
-  		          hasName = true;
-  	          }
+
       	      
-      	      if (answer.equals("No") || answer.equals("no")){
-      	    	  System.out.println("\nWhy not!!! Maybe because you don't have imagnation or not Good to pick a name!!??");
-      	      }else if(answer.equals("I don't know") || answer.equals("")) {
-      	    	  
-      	      }
+
 
           }
 
